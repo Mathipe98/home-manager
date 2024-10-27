c5f68c3e15a3fc0cf407756e951cb8f84064f9cb
Mathias Pettersen <mathias.pettersen@proton.me>
2024-10-27 17:48:35 +0100

    Added nvim config

--- home.nix
+++ home.nix
@@ -12,6 +12,8 @@
     pkgs.fortune
   ];
 
+  imports = ["./nix-configs/nvimImport.nix"]
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
