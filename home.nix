48b99608b96a7102abd12806a693ae4eef1fe72e
Mathias Pettersen <mathias.pettersen@proton.me>
2024-10-27 18:04:32 +0100

    Added new config

--- home.nix
+++ home.nix
@@ -1,43 +1,16 @@
-{ config, pkgs, ... }:
-
+{ lib, pkgs, ... }:
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "mathipe";
-  home.homeDirectory = "/home/mathipe";
-
-  # Packages that should be installed to the user profile.
-  home.packages = [
-    pkgs.htop
-    pkgs.fortune
-  ];
-
-  imports = ["./nix-configs/nvimImport.nix"]
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "24.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  programs.emacs = {
-    enable = true;
-    extraPackages = epkgs: [
-      epkgs.nix-mode
-      epkgs.magit
+  home = {
+    packages = with pkgs; [
+      hello
     ];
-  };
 
-  services.gpg-agent = {
-    enable = true;
-    defaultCacheTtl = 1800;
-    enableSshSupport = true;
+    # This needs to actually be set to your username
+    username = "mathipe";
+    homeDirectory = "/home/mathipe";
+
+    # You do not need to change this if you're reading this in the future.
+    # Don't ever change this after the first build.  Don't ask questions.
+    stateVersion = "23.11";
   };
 }
